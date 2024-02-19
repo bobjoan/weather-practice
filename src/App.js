4802d98ab223ed413d153c9979d877ddd202166d
bobjoan <bobjoan966@gmail.com>
2024-02-19 13:38:45 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/bobjoan" target="_blank" rel="noreferrer">
+          Joan Bob
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/bobjoan/searchEngine"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
+}
+export default App;
