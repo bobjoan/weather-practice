da0bf33ef03ee91af5bfb18a39090afcdeff7d05
bobjoan <bobjoan966@gmail.com>
2024-02-20 14:03:20 +0100

    Added formatted date

---
+++ src/FormattedDate.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function FormattedDate(props) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tusday",
+    "Wenesday",
+    "Thusday",
+    "Friay",
+    "Satrday",
+  ];
+  let day = props.date.getDay;
+  let hours = props.date.getHours;
+  let minutes = props.date.getMinutes;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return (
+    <div>
+      {day} {hours}:{minutes}
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -46,7 +47,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherInfo.city}</h1>
         <ul>
-          <li>{weatherInfo.date}</li>
+          <li>
+            <FormattedDate date={weatherInfo.date} />
+          </li>
           <li>{weatherInfo.description}</li>
         </ul>
         <div className="row">
