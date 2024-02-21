e33a58947ee53b69fae4a4acf72acebe1f083936
bobjoan <bobjoan966@gmail.com>
2024-02-21 16:20:38 +0100

    Added a container to form

--- src/Weather.js
+++ src/Weather.js
@@ -35,17 +35,25 @@ export default function Weather(props) {
 
   if (weatherInfo.loader) {
     return (
-      <div className="Weather">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="search"
-            placeholder="Enter a city"
-            required
-            onChange={updateCity}
-          />
-          <input type="submit" value="Search" />
-        </form>
-        <WeatherUpdate info={weatherInfo} />
+      <div className="container">
+        <div className="Weather">
+          <form onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Enter a city"
+                  required
+                  onChange={updateCity}
+                />
+              </div>
+              <div className="col-3">
+                <input type="submit" value="Search" />
+              </div>
+            </div>
+          </form>
+          <WeatherUpdate info={weatherInfo} />
+        </div>
       </div>
     );
   } else {
